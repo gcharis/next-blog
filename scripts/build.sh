6444c1bf37b2916223e7abf6da65756eaaac864f
Charis Giaralis <charis.giaralis@gmail.com>
2021-03-14 00:49:29 +0200

    Docker everything
    Improvements
    
    Automate and dockerized
    
    Remove unnecessary html

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,47 +1,58 @@
+LOG_FILE=logfile.log
+LIGHT_GREEN='\033[1;32m'
+RED='\033[0;31m'
+NO_COLOUR='\033[0m'
+
+# Clear log file
+> ${LOG_FILE}
+
+# Log everything by default
+exec 3>&1 1>>${LOG_FILE} 2>&1
+
 function createNetwork () {
-  docker network create $1 > logfile.log
+  docker network create $1
 }
 
 function getNetwork (){
-  docker network inspect $1 > logfile.log
+  docker network inspect $1
 }
 
 superNetwork="react2react"
 
 if (getNetwork ${superNetwork}); then
-  echo "network ${superNetwork} is on"
+  echo -e "${LIGHT_GREEN}network ${superNetwork} is ok!${NO_COLOUR}" | tee /dev/fd/3
 else
-  echo "creating network ${superNetwork}..."
-  createNetwork ${superNetwork}
+  echo "creating network ${superNetwork}..." | tee /dev/fd/3
+  createNetwork ${superNetwork} 
+  echo -e "${LIGHT_GREEN}network ${superNetwork} is ok!${NO_COLOUR}" | tee /dev/fd/3
 fi
 
-echo "Stopping strapi-blog..."
-docker stop strapi-blog > logfile.log || true && docker rm strapi-blog > logfile.log || true
-
-echo "Starting strapi-blog..."
+echo "Starting api..." | tee /dev/fd/3
+docker stop strapi-blog || true && docker rm strapi-blog || true
 docker run -dp 1337:1337 --network ${superNetwork} --network-alias charis-blog --name strapi-blog strapi-blog
 
+# Wait for the api to be functional
 retries=0
-while ! curl "http://localhost:1337" > logfile.log
+while ! curl "http://localhost:1337"
 do 
   echo "welp"
   sleep 2
   retries=$((retries+1))
 
   if ((retries > 10)); then
-    echo "Something went wrong with the api!"
+    echo -e "${RED}Something went wrong with the api. ${NO_COLOUR}"
     exit 125
   fi
 done
 
-echo "strapi-blog started!"
+echo -e "${LIGHT_GREEN}Api started successfully!${NO_COLOUR}" | tee /dev/fd/3
 
-echo "Building next app..."
-
+echo "Building next app..." | tee /dev/fd/3
 docker build --network host -t gcharis/react2react .
+echo -e "${LIGHT_GREEN}App built successfully!${NO_COLOUR}" | tee /dev/fd/3
 
-echo "Stopping existing next app"
-docker stop react2react > logfile.log || true && docker rm react2react > logfile.log || true
+echo "Starting next app" | tee /dev/fd/3
+docker stop react2react || true && docker rm react2react || true
+docker run -e API_HOST=charis-blog -dp 3000:3000 --network ${superNetwork} --name react2react gcharis/react2react
+echo -e "${LIGHT_GREEN}App started successfully!${NO_COLOUR}" | tee /dev/fd/3
 
-echo "Starting next app"
-docker run -e API_HOST=charis-blog -dp 3000:3000 --network ${superNetwork} --name react2react gcharis/react2react 
