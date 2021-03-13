ab98f7f368643dcaa97a73ffdd28ddf899bff1fb
Charis Giaralis <charis.giaralis@gmail.com>
2021-03-14 01:27:09 +0200

    Fix tests

--- __tests__/index.spec.tsx
+++ __tests__/index.spec.tsx
@@ -3,6 +3,8 @@ import PostsPage from '../pages';
 import React from 'react';
 import { Post } from '../lib/posts/post';
 import Link from 'next/link';
+import PostItem from '../lib/posts/post-item.component';
+import { CardContent, CardHeader } from '@material-ui/core';
 
 describe('PostsPage', () => {
   it('renders without crashing', () => {
@@ -11,31 +13,51 @@ describe('PostsPage', () => {
     expect(wrapper).toBeTruthy();
   });
 
-  it('should display a list of posts', () => {
-    const posts = [
-      {
-        id: '1',
-        title: 'title1',
-      },
-      {
-        id: '2',
-        title: 'title2',
-      },
-    ] as Post[];
-
-    const wrapper = mount(<PostsPage posts={posts} />);
-    const postsElements = wrapper.find('article');
-
-    postsElements.forEach((el, i) => {
-      expect(el.find(Link).prop('href')).toStrictEqual({ pathname: `/posts/${posts[i].id}` });
-      expect(el.find(Link).text()).toBe(posts[i].title);
+  describe('rendered post list', () => {
+    let posts: Post[];
+    beforeEach(() => {
+      posts = [
+        {
+          id: '1',
+          title: 'title1',
+          author: {
+            id: '1',
+            username: 'charis',
+          },
+          content: 'Super post',
+        },
+        {
+          id: '2',
+          title: 'title2',
+          author: {
+            id: '1',
+            username: 'charis',
+          },
+          content: 'Super post',
+        },
+      ] as Post[];
     });
-    expect(postsElements.length).toBe(2);
-  });
 
-  it('should have a link to go to the users posts', () => {
-    const wrapper = shallow(<PostsPage posts={[]} />);
+    it('should display the correct length of items', () => {
+      const wrapper = mount(<PostsPage posts={posts} />);
+      const postItems = wrapper.find(PostItem);
 
-    expect(wrapper.find(Link).prop('href')).toStrictEqual({ pathname: `/user/posts` });
+      expect(postItems.length).toBe(2);
+    });
+
+    it('rendered post items should contain one link to the user and one to the post', () => {
+      const wrapper = mount(<PostsPage posts={posts} />);
+      const postItems = wrapper.find(PostItem);
+
+      postItems.forEach((el, i) => {
+        const post = posts[i];
+        expect(el.find(CardHeader).find(Link).prop('href')).toStrictEqual(
+          `/${post.author.username}`,
+        );
+        expect(el.find(CardContent).find(Link).prop('href')).toStrictEqual(
+          `/${post.author.username}/${post.id}`,
+        );
+      });
+    });
   });
 });
